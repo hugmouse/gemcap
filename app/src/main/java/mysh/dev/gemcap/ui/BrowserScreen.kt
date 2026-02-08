hugmouse/gemcap

commit ea90d4f9c6ab8ec56c12a0e91d7d867b5103c874

fix(BrowserScreen): center gemtext on big screens Sort of like margin: 0 auto; in the web

app/src/main/java/mysh/dev/gemcap/ui/BrowserScreen.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -562,7 +564,7 @@ private fun GeminiContentList(
                 LazyColumn(
                     state = listState,
                     modifier = Modifier
-                        .width(840.dp) // TODO: make configurable
+                        .fillMaxWidth()
                         .padding(horizontal = 16.dp),
                     contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp),
                     verticalArrangement = Arrangement.spacedBy(6.dp)
@@ -571,15 +573,26 @@ private fun GeminiContentList(
                         items = content, key = { _, item -> item.id }) { index, item ->
                         val highlight =
                             searchState.query.isNotBlank() && index == searchState.currentResultIndex
-                        ContentItem(
-                            item = item,
-                            styles = cachedStyles,
-                            searchQuery = searchState.query,
-                            highlight = highlight,
-                            onLinkClick = { callbacks.onLinkClick(it) },
-                            onOpenImageInNewTab = { callbacks.onOpenImageInNewTab(it) },
-                            onCopyLink = { callbacks.onCopyLink(it) },
-                            onOpenInNewTab = { callbacks.onOpenInNewTab(it) })
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.TopCenter
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(840.dp)
+                            ) { // TODO: make configurable
+                                ContentItem(
+                                    item = item,
+                                    styles = cachedStyles,
+                                    searchQuery = searchState.query,
+                                    highlight = highlight,
+                                    onLinkClick = { callbacks.onLinkClick(it) },
+                                    onOpenImageInNewTab = { callbacks.onOpenImageInNewTab(it) },
+                                    onCopyLink = { callbacks.onCopyLink(it) },
+                                    onOpenInNewTab = { callbacks.onOpenInNewTab(it) }
+                                )
+                            }
+                        }
                     }
                 }
             }

commit 9b4920f3bee8b7e437555c5f2317f6aa7edf9df2

fix(UI): return 840.dp width limit Regression from "ea90d4f9c6ab8ec56c12a0e91d7d867b5103c874"

app/src/main/java/mysh/dev/gemcap/ui/BrowserScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -593,8 +594,7 @@ private fun GeminiContentList(
                         ) {
                             Box(
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                                    .widthIn(max = 840.dp)
+                                    .width(840.dp)
                             ) { // TODO: make configurable
                                 ContentItem(
                                     item = item,