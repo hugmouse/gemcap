76276994be994342dc5f237bbed6efb466f8d886
Mysh <me@mysh.dev>
2026-02-24 18:24:20 +0100

    refactor: consolidate ContentItem callbacks into ContentActions

--- app/src/main/java/mysh/dev/gemcap/ui/content/ContentItem.kt
+++ app/src/main/java/mysh/dev/gemcap/ui/content/ContentItem.kt
@@ -2,6 +2,17 @@ package mysh.dev.gemcap.ui.content
 
 import androidx.compose.runtime.Composable
 import mysh.dev.gemcap.domain.GeminiContent
+import mysh.dev.gemcap.domain.StableByteArray
+
+data class ContentActions(
+    val onLinkClick: (String) -> Unit,
+    val onOpenImageInNewTab: (String) -> Unit,
+    val onCopyLink: (String) -> Unit,
+    val onOpenInNewTab: (String) -> Unit,
+    val onLoadEmbeddedMedia: (Int) -> Unit,
+    val onCollapseEmbeddedMedia: (Int) -> Unit,
+    val onDownloadEmbeddedMedia: (String, StableByteArray, String) -> Unit
+)
 
 /**
  * Dispatcher composable that renders the appropriate content type
@@ -12,13 +23,7 @@ fun ContentItem(
     styles: CachedTextStyles,
     searchQuery: String,
     highlight: Boolean,
-    onLinkClick: (String) -> Unit,
-    onOpenImageInNewTab: (String) -> Unit,
-    onCopyLink: (String) -> Unit,
-    onOpenInNewTab: (String) -> Unit,
-    onLoadEmbeddedMedia: (Int) -> Unit,
-    onCollapseEmbeddedMedia: (Int) -> Unit,
-    onDownloadEmbeddedMedia: (String, ByteArray, String) -> Unit
+    actions: ContentActions
 ) {
     when (item) {
         is GeminiContent.Heading -> HeadingContent(item, styles, searchQuery)
@@ -26,24 +31,24 @@ fun ContentItem(
             item,
             styles,
             searchQuery,
-            onLinkClick,
-            onCopyLink,
-            onOpenInNewTab
+            actions.onLinkClick,
+            actions.onCopyLink,
+            actions.onOpenInNewTab
         )
 
         is GeminiContent.Text -> TextContent(item, searchQuery)
         is GeminiContent.ListItem -> ListItemContent(item, styles, searchQuery)
         is GeminiContent.Quote -> QuoteContent(item, styles, searchQuery)
         is GeminiContent.Preformatted -> PreformattedContent(item, styles, searchQuery, highlight)
-        is GeminiContent.Image -> ImageContent(item, onOpenImageInNewTab)
+        is GeminiContent.Image -> ImageContent(item, actions.onOpenImageInNewTab)
         is GeminiContent.EmbeddedMedia -> EmbeddedMediaContent(
             item = item,
             styles = styles,
-            onLoadMedia = onLoadEmbeddedMedia,
-            onCollapseMedia = onCollapseEmbeddedMedia,
-            onOpenInNewTab = onOpenInNewTab,
-            onCopyLink = onCopyLink,
-            onDownloadMedia = onDownloadEmbeddedMedia
+            onLoadMedia = actions.onLoadEmbeddedMedia,
+            onCollapseMedia = actions.onCollapseEmbeddedMedia,
+            onOpenInNewTab = actions.onOpenInNewTab,
+            onCopyLink = actions.onCopyLink,
+            onDownloadMedia = actions.onDownloadEmbeddedMedia
         )
     }
 }
